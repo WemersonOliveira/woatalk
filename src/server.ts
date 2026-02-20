1b024689b60200d6efdc622d87de9d3b5cf32108
Wemerson Oliveira <wemerson.contacto@gmail.com>
2026-02-20 18:44:54 -0300

    add vercel routing

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,3 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
+import app from "../api/index.js"
 
-dotenv.config()
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.get("/", (_req, res) => {
-  res.send("WOA TALK API ONLINE 🚀")
-})
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000")
-})
+export default app

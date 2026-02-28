215b289fdbce66f59b38fb09f0d1193f1cf1e51e
Wemerson Oliveira <wemerson.contacto@gmail.com>
2026-02-28 13:45:15 -0300

    fix: add root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'WOA Talk',
+  description: 'Community Platform',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
